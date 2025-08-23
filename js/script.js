79fbeb8fa2b2de542b8af6c00f665b40572a3c8c
Ariadna Arranz <sparapinzy@gmail.com>
2025-08-23 01:34:53 -0700

    add css and js folders

---
+++ js/script.js
@@ -0,0 +1,41 @@
+let currentSlide = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slides img').length;
+let slideInterval = setInterval(nextSlide, 4000); // 4 seconds
+
+document.querySelector('.next').addEventListener('click', () => {
+    nextSlide();
+    resetInterval();
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+    prevSlide();
+    resetInterval();
+});
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    updateCarousel();
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    updateCarousel();
+}
+
+function updateCarousel() {
+    slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+function resetInterval() {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, 4000);
+}
+
+// Optional: Pause on hover
+document.querySelector('.carousel').addEventListener('mouseenter', () => {
+    clearInterval(slideInterval);
+});
+document.querySelector('.carousel').addEventListener('mouseleave', () => {
+    slideInterval = setInterval(nextSlide, 4000);
+});
